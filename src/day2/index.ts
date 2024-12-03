a18eac7ed6ec811c99f6ed601f8d5cabaf035d72
Ryan <deadlyryz@hotmail.co.uk>
2024-12-03 23:45:38 +0000

    feat: code cleanup

--- src/day2/index.ts
+++ src/day2/index.ts
@@ -13,42 +13,37 @@ const parseInput = (input: String) => {
   });
 };
 
+const isRowSafe = (row: number[]) => {
+  let isIncreasing: null | boolean = null;
+  for (let i = 0; i < row.length - 1; i++) {
+    const currentNum = row[i];
+    const nextNum = row[i + 1];
+
+    const difference = currentNum - nextNum;
+    const differenceAbs = Math.abs(currentNum - nextNum);
+
+    if (difference === 0 || differenceAbs > 3) {
+      return false;
+    }
+
+    if (isIncreasing === null) {
+      isIncreasing = difference > 0;
+    }
+
+    const nextIsIncreasing = difference > 0;
+
+    if (isIncreasing !== nextIsIncreasing) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
 const part1 = () => {
   const rows = parseInput(input);
 
-  const parsedRows = rows.filter((row) => {
-    let isIncreasing: null | boolean = null;
-
-    const getIsSafe = () => {
-      for (let i = 0; i < row.length - 1; i++) {
-        const currentNum = row[i];
-        const nextNum = row[i + 1];
-
-        const difference = currentNum - nextNum;
-        const differenceAbs = Math.abs(currentNum - nextNum);
-
-        if (difference === 0 || differenceAbs > 3) {
-          return false;
-        }
-
-        if (isIncreasing === null) {
-          isIncreasing = difference > 0;
-        }
-
-        const nextIsIncreasing = difference > 0;
-
-        if (isIncreasing !== nextIsIncreasing) {
-          return false;
-        }
-      }
-
-      return true;
-    };
-
-    const isSafe = getIsSafe();
-
-    return isSafe;
-  });
+  const parsedRows = rows.filter(isRowSafe);
 
   return parsedRows.length;
 };
@@ -57,37 +52,9 @@ const part2 = () => {
   const rows = parseInput(input);
 
   const parsedRows = rows.filter((row) => {
-    const isRowSafe = (innerRow: number[]) => {
-      let isIncreasing: null | boolean = null;
-
-      for (let i = 0; i < innerRow.length - 1; i++) {
-        const currentNum = innerRow[i];
-        const nextNum = innerRow[i + 1];
-
-        const difference = currentNum - nextNum;
-        const differenceAbs = Math.abs(currentNum - nextNum);
-
-        if (difference === 0 || differenceAbs > 3) {
-          return false;
-        }
-
-        if (isIncreasing === null) {
-          isIncreasing = difference >= 0;
-        }
-
-        const nextIsIncreasing = difference >= 0;
-
-        if (isIncreasing !== nextIsIncreasing) {
-          return false;
-        }
-      }
-
-      return true;
-    };
-
-    function getRowPermutations(arr: number[]) {
+    const getRowPermutations = (arr: number[]) => {
       return arr.map((_, idx) => arr.toSpliced(idx, 1));
-    }
+    };
 
     return isRowSafe(row) || getRowPermutations(row).some(isRowSafe);
   });
