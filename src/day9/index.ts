deeaf9432efe8160113ac48ce76f5233c31e61f4
Ryan <deadlyryz@hotmail.co.uk>
2024-12-11 22:45:44 +0000

    feat: day 9 part 2 progress

--- src/day9/index.ts
+++ src/day9/index.ts
@@ -1,6 +1,6 @@
 import { getInput } from "../utils/get-input.ts";
 
-const input = getInput(import.meta.dirname, "./input.txt");
+const input = getInput(import.meta.dirname, "./input.example.txt");
 
 const getParsedInput = (input: string) => {
   const numbers = input.trim().split("").map(Number);
@@ -16,25 +16,28 @@ const getParsedInput = (input: string) => {
     if (number === 0) return;
 
     if (isFile) {
-      output.push(...Array(number).fill(String(id)));
+      const fileBlock = Array(number).fill(String(id));
+      output.push(...fileBlock);
       id++;
     } else if (isFreeSpace) {
-      output.push(...Array(number).fill("."));
+      const freeSpaceBlock = Array(number).fill(".");
+      output.push(...freeSpaceBlock);
     }
   });
 
   return output;
 };
 
-const getIndexOfFirstFreeSpace = (input: string[]) => {
+const getIndexesOfFreeSpace = (input: string[]) => {
+  const indexes = [];
   for (let i = 0; i < input.length; i++) {
     const item = input[i];
     if (item === ".") {
-      return i;
+      indexes.push(i);
     }
   }
 
-  return;
+  return indexes;
 };
 
 const swapItems = (
@@ -57,15 +60,19 @@ const checksum = (input: string[]) => {
 };
 
 const swapNumbers = (parsedInput: string[]) => {
-  for (let i = parsedInput.length - 1; i > 0; i--) {
-    const indexOfFirstFreeSpace = getIndexOfFirstFreeSpace(parsedInput);
+  const indexesOfFreeSpace = getIndexesOfFreeSpace(parsedInput);
+  let freeSpaceIndex = 0;
 
+  for (let i = parsedInput.length - 1; i > 0; i--) {
     const item = parsedInput[i];
 
     if (item === ".") continue;
 
+    const indexOfFirstFreeSpace = indexesOfFreeSpace[freeSpaceIndex];
+
     if (indexOfFirstFreeSpace && i > indexOfFirstFreeSpace) {
       swapItems(parsedInput, i, indexOfFirstFreeSpace);
+      freeSpaceIndex++;
     } else {
       return;
     }
@@ -80,9 +87,105 @@ const part1 = () => {
   return checksum(parsedInput);
 };
 
+const getParsedInputPart2 = (input: string) => {
+  const numbers = input.trim().split("").map(Number);
+
+  const output: string[][] = [];
+
+  let id = 0;
+
+  numbers.forEach((number, i) => {
+    const isFile = i % 2 === 0;
+    const isFreeSpace = !isFile;
+
+    if (number === 0) return;
+
+    if (isFile) {
+      const fileBlock = Array(number).fill(String(id));
+      output.push(fileBlock);
+      id++;
+    } else if (isFreeSpace) {
+      const freeSpaceBlock = Array(number).fill(".");
+      output.push(freeSpaceBlock);
+    }
+  });
+
+  return output;
+};
+
+const getIndexesOfFreeSpacePart2 = (input: string[][]) => {
+  const indexes = [];
+  for (let i = 0; i < input.length; i++) {
+    const item = input[i];
+    if (item.includes(".")) {
+      indexes.push(i);
+    }
+  }
+
+  return indexes;
+};
+
+const swapItemsPart2 = (
+  input: string[][],
+  item: string[],
+  currentIndex: number,
+  nextIndex: number
+) => {
+  input[currentIndex] = input[currentIndex].map(() => {
+    return ".";
+  });
+  let itemIndex = 0;
+  input[nextIndex] = input[nextIndex].map((freeSpace) => {
+    if (freeSpace !== ".") {
+      return freeSpace;
+    }
+
+    const value = item[itemIndex];
+    itemIndex++;
+    return value ?? freeSpace;
+  });
+};
+
 const part2 = () => {
-  const parsedInput = getParsedInput(input);
+  const parsedInput = getParsedInputPart2(input);
+
+  const indexesOfFreeSpace = getIndexesOfFreeSpacePart2(parsedInput);
+
+  for (let i = parsedInput.length - 1; i > 0; i--) {
+    const item = parsedInput[i];
+
+    if (indexesOfFreeSpace.includes(i)) continue;
+
+    checkFreeSpaces(item, i);
+  }
+
+  function checkFreeSpaces(item: string[], i: number) {
+    for (
+      let freeSpaceIndex = 0;
+      freeSpaceIndex < indexesOfFreeSpace.length;
+      freeSpaceIndex++
+    ) {
+      const freeSpace = parsedInput[freeSpaceIndex];
+      const freeSpaceLeft = freeSpace.filter((space) => {
+        return space === ".";
+      }).length;
+
+      if (freeSpaceLeft >= item.length) {
+        swapItemsPart2(parsedInput, item, i, freeSpaceIndex);
+        return;
+      }
+    }
+  }
+  return checksum(
+    parsedInput
+      .map((group) => group.join(""))
+      .join("")
+      .split("")
+  );
 };
 
 console.log(part1());
-// console.log(part2());
+console.log(part2());
+
+// 00992111777.44.333....5555.6666.....8888..
+// 00992111777.44.333....5555.6666.....8888..
